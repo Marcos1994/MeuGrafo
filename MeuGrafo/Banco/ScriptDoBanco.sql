28838c6bf69860fcad7bde5199765de26580673e
Marcos <marcos.antas@hotmail.com.br>
2015-02-26 13:57:20 -0300

    Diagramas OK

---
+++ MeuGrafo/Banco/ScriptDoBanco.sql
@@ -0,0 +1,26 @@
+--CREATE DATABASE GrafoWebService;
+USE GrafoWebService;
+
+CREATE TABLE tb_Grafo
+(
+	id_grafo INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(30) UNIQUE NOT NULL,
+	width INT NOT NULL,
+	height INT NOT NULL
+);
+
+CREATE TABLE tb_Vertice
+(
+	id_vertice INT PRIMARY KEY IDENTITY(1,1),
+	id_grafo INT FOREIGN KEY REFERENCES tb_Grafo(id_grafo) NOT NULL,
+	posX INT NOT NULL,
+	posY INT NOT NULL
+);
+
+CREATE TABLE tb_Aresta
+(
+	id_aresta INT PRIMARY KEY IDENTITY(1,1),
+	id_origem INT FOREIGN KEY REFERENCES tb_Vertice(id_vertice) NOT NULL,
+	id_destino INT FOREIGN KEY REFERENCES tb_Vertice(id_vertice) NOT NULL,
+	peso INT NOT NULL
+)
