0c0fc5fec73ae0d48f658519c35d5ce8e06a5d6b
Marcos <marcos.antas@hotmail.com.br>
2015-02-26 14:47:44 -0300

    Criação dos métodos de GrafoWSA

--- MeuGrafo/GrafoWebService/GrafoWebService/GrafoWSA.asmx.cs
+++ MeuGrafo/GrafoWebService/GrafoWebService/GrafoWSA.asmx.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Web.Services;
 
-namespace GrafoWebService
+namespace GrafoWebService.GrafoWebService
 {
 	/// <summary>
 	/// Summary description for WebService1
@@ -17,10 +18,77 @@ namespace GrafoWebService
 	public class WebService1 : System.Web.Services.WebService
 	{
 
+		/*_________ Grafo _________*/
+
 		[WebMethod]
-		public string HelloWorld()
+		public string criarGrafo()
 		{
-			return "Hello World";
+			JavaScriptSerializer js = new JavaScriptSerializer();
+			return js.Serialize(new Retorno());
+		}
+
+		[WebMethod]
+		public string salvarGrafo(int idGrafo, string posicoesVertices)
+		{
+			JavaScriptSerializer js = new JavaScriptSerializer();
+			return js.Serialize(new Retorno());
+		}
+
+		[WebMethod]
+		public string abrirGrafo(string nomeGrafo)
+		{
+			JavaScriptSerializer js = new JavaScriptSerializer();
+			return js.Serialize(new Retorno());
+		}
+
+
+
+		/*_________ Vertices _________*/
+
+		[WebMethod]
+		public string criarVertice(string nome)
+		{
+			JavaScriptSerializer js = new JavaScriptSerializer();
+			return js.Serialize(new Retorno());
+		}
+
+		[WebMethod]
+		public string removerVertice(int idVertice)
+		{
+			JavaScriptSerializer js = new JavaScriptSerializer();
+			return js.Serialize(new Retorno());
+		}
+
+		[WebMethod]
+		public string alterarValor(int idVertice, string valor)
+		{
+			JavaScriptSerializer js = new JavaScriptSerializer();
+			return js.Serialize(new Retorno());
+		}
+
+
+
+		/*_________ Arestas _________*/
+
+		[WebMethod]
+		public string criarAresta(int idOrigem, int idDestino, int peso)
+		{
+			JavaScriptSerializer js = new JavaScriptSerializer();
+			return js.Serialize(new Retorno());
+		}
+
+		[WebMethod]
+		public string removerAresta(int idAresta)
+		{
+			JavaScriptSerializer js = new JavaScriptSerializer();
+			return js.Serialize(new Retorno());
+		}
+
+		[WebMethod]
+		public string alterarPeso(int idAresta, string valor)
+		{
+			JavaScriptSerializer js = new JavaScriptSerializer();
+			return js.Serialize(new Retorno());
 		}
 	}
 }
---
+++ MeuGrafo/GrafoWebService/GrafoWebService/Retorno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GrafoWebService.GrafoWebService
+{
+	public class Retorno
+	{
+		public string retorno { get; set; }
+
+		public Retorno()
+		{
+			this.retorno = "ACK";
+		}
+	}
+}
