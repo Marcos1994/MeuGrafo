1edcb4a2d4859a74071fd22052b422e6b046d5f0
Marcos <marcos.antas@hotmail.com.br>
2015-02-26 14:35:04 -0300

    Classes de GrafoPlus criadas

---
+++ MeuGrafo/GrafoWebService/GrafoPlus/ArestaPlus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MeuGrafo;
+
+namespace GrafoWebService.GrafoPlus
+{
+	public class ArestaPlus : Aresta
+	{
+		public int idAresta { get; set; }
+	}
+}
---
+++ MeuGrafo/GrafoWebService/GrafoPlus/VerticePlus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MeuGrafo;
+
+namespace GrafoWebService.GrafoPlus
+{
+	public class VerticePlus : Vertice
+	{
+		public int idVertice { get; set; }
+		public int posX { get; set; }
+		public int posY { get; set; }
+	}
+}
