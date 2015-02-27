eae5105c157aa1ea4e7c6ea12bc9339d29d25858
Marcos <marcos.antas@hotmail.com.br>
2015-02-26 21:17:26 -0300

    Escrever Labirinto

--- MeuGrafo/MeuGrafo/Program.cs
+++ MeuGrafo/MeuGrafo/Program.cs
@@ -8,13 +8,31 @@ namespace MeuGrafo
 {
 	class Program
 	{
+		private static int width = 10;
+		private static int height = 12;
+		private static int[,] labirinto;
+		private static string labirintoString = "111111111110000000011200010101111101010110000001011011111103100000000111111111013100100001110100111110000003011111111111";
+		private static string opcao = "";
+		private static string valor = "";
+		private static Grafo grafo;
+
 		static void Main(string[] args)
 		{
-			int width = 10;
-			int height = 12;
-			int[,] labirinto = gerarLabirintoInicial();
-			string opcao = "-1";
-			string valor = "";
+			grafo = new Grafo();
+			Console.WriteLine("Labirinto:");
+			escreverLabirinto();
+			Console.ReadKey();
+			Console.BackgroundColor = ConsoleColor.Black;
+			Console.Clear();
+			//aqui vai ser gerado o menor caminho para poder ser escrito
+			Console.WriteLine("Solucao");
+			escreverLabirinto();
+			Console.ReadKey();
+		}
+
+		private static void escreverLabirinto()
+		{
+			gerarLabirintoInicial();
 			for (int i = 0; i < height; i++)
 			{
 				for (int j = 0; j < width; j++)
@@ -34,24 +52,28 @@ namespace MeuGrafo
 						case "3":
 							Console.BackgroundColor = ConsoleColor.Red;
 							break;
+						case "4":
+							Console.BackgroundColor = ConsoleColor.Green;
+							break;
 						default:
-							Console.BackgroundColor = ConsoleColor.White;
+							Console.BackgroundColor = ConsoleColor.DarkYellow;
 							break;
 					}
 					Console.Write(" ");
 				}
 				Console.WriteLine();
 			}
-			Console.ReadKey();
 		}
 
-		private static int[,] gerarLabirintoInicial()
+		private static void gerarLabirintoInicial()
 		{
-			int[,] labirinto = new int[12, 10];
-			string labirintoString = "111111111110000000011200010101111101010110000001011011111103100000000111111111013100100001110100111110000003011111111111";
+			labirinto = new int[height, width];
 			for (int i = 0; i < labirintoString.Length; i++)
-				labirinto[(i / 10), (i % 10)] = int.Parse(labirintoString.Substring(i, 1));
-			return labirinto;
+			{
+				if (labirintoString.Substring(i, 1) != "1")
+					grafo.vertices.Add(new VerticeComID(valor, i));
+				labirinto[(i / width), (i % width)] = int.Parse(labirintoString.Substring(i, 1));
+			}
 		}
 	}
 }
---
+++ MeuGrafo/MeuGrafo/VerticeComID.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuGrafo
+{
+	class VerticeComID : Vertice
+	{
+		public int id { get; set; }
+
+		public VerticeComID(Object valor, int id)
+			: base(valor)
+		{
+			this.id = id;
+		}
+	}
+}
